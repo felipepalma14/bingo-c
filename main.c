83802ceba2653cb768d1a8d7c529410436c77c19
Felipe Palma <felipepalma14@gmail.com>
2025-05-28 20:14:14 -0400

    Add insert method

--- main.c
+++ main.c
@@ -1,6 +1,38 @@
+#include <stdbool.h>
 #include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * Estrutura base da matriz
+ */
+typedef struct {
+    int value;
+    bool marked;
+    struct TreeNode *left, *right;
+} TreeNode;
+
+/*
+ * Inserir na arvore
+ */
+TreeNode* insert(TreeNode *root, int num) {
+    // adiciona se for null a raiz
+    if(root == NULL) {
+        TreeNode *node = (TreeNode*) malloc(sizeof(TreeNode));
+        node->value = num;
+        node->left = NULL;
+        node->right = NULL;
+        return node;
+    }
+    if(num < root->value){
+        root->left = insert(root->left, num);
+    }
+    else {
+        root->right = insert(root->right, num);
+    }
+    return root;
+}
 
 int main(void) {
-    printf("Hello, World!\n");
+
     return 0;
 }
